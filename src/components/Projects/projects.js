a017377d8e832e11f352065383b3f115d454070c
Calvin Yap <calvin.yap@ryerson.ca>
2019-04-23 10:06:12 -0400

    new changes

--- src/components/Projects/projects.js
+++ src/components/Projects/projects.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import './projects.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 class Projects extends React.Component{
   render(){
@@ -7,7 +9,42 @@ class Projects extends React.Component{
     //document.body.style.backgroundAttachment = "fixed";
         return (
       <div>
-      <p> hi </p>
+
+        <div className="box">
+          <h1 className="titleprojects"> Projects </h1>
+          <h1 className="projectlinks">Java</h1>
+          <h1 className="projectlinks">Web Development</h1>
+          <h1 className="projectlinks">Hackathons</h1>
+          <h1 className="projectlinks">More</h1>
+        </div>
+
+
+        <div className="javaBox">
+          <h1 className="subLinks">How I Started Coding</h1>
+        </div>
+        <div className="javaBox">
+          <h1 className="subLinks">Java</h1>
+        </div>
+        <div className="javaBox">
+          <h1 className="subLinks">Web Development</h1>
+        </div>
+        <div className="javaBox">
+          <h1 className="subLinks">Hackathons</h1>
+        </div>
+        <div className="javaBox">
+          <h1 className="subLinks">More</h1>
+        </div>
+        
+        <footer>
+        <a className="bottomlinks" href="https://github.com/Calvin-Yap"><FontAwesomeIcon className="fontawesome" icon={['fab', 'github']} size="lg" /></a>
+        <a className="bottomlinks" href="linkedman"><FontAwesomeIcon className="fontawesome" icon={['fab', 'linkedin']} size="lg" /></a>
+        <a className="bottomlinks" href="linkedman"><FontAwesomeIcon className="fontawesome" icon={['fas', 'file']} size="lg" /></a>
+        <a className="bottomlinks" href="linkedman"><FontAwesomeIcon className="fontawesome"icon={['fas', 'envelope']} size="lg" /></a>
+        <br/>
+        <br/>
+        <p>Made by Calvin Yap </p>
+        <p>&copy; 2018 Calvin Yap</p>
+        </footer>
     </div>
     )
   }
